b1ee9313cb22bbe18f6e575eeb9a56718727a0cd
Fluxlus <133106820+pandhu-rendra@users.noreply.github.com>
2024-02-29 13:32:50 +0700

    Update Rules.js

--- JS/Rules.js
+++ JS/Rules.js
@@ -6,7 +6,7 @@ const copyBtn = document.getElementById("copy-btn");
 
 copyBtn.addEventListener("click", function(event) {
     event.preventDefault();
-    var ip = "jogar.minecket.net"; // Add your server's IP here
+    var ip = "play.cosmicnetwork.xyz"; // Add your server's IP here
     navigator.clipboard.writeText(ip).then(() => {
         var popup = document.getElementById("play-copy-alert");
 
